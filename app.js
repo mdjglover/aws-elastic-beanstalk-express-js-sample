ae28620e122f8ff632dde595a0026f72716a480a
Mike Glover <not@right.now>
2021-05-01 16:22:34 +0100

    Updated message again

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, is it me you\'re looking for?'));
+const message = 'Hello, is it me you\'re looking for? I can see it in your eyes, I can see it in your smile. You\'re all that I ever wanted and my arms are open wide.'
+
+app.get('/', (req, res) => res.send(message));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
